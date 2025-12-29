8440b6b9084ac152782101292b628c9f52a2959c
Jeff Masty <jeffmasty@gmail.com>
2025-12-28 19:15:51 -0700

    Initial commit

---
+++ src/main/java/net/judahzone/FirstTry.java
@@ -0,0 +1,12 @@
+package net.judahzone;
+
+import judahzone.util.Recording;
+
+public class FirstTry {
+
+	public static void main(String[] args) {
+		new Recording();
+		System.out.println(FirstTry.class.getCanonicalName());
+	}
+
+}
